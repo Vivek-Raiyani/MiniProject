b7c1e4b987fc0faf5ae896cbe1058e27a553add7
Vivek-Raiyani <vivekraiyani123@gmail.com>
2024-03-17 11:59:26 +0530

    created the pages files that need to creates ,but they are empty at the moment

--- square/square/settings.py
+++ square/square/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +47,11 @@ INSTALLED_APPS = [
     'reviews',
     'bootstrap5',
 ]
+
+# for media file accesing purpose ,need to learn how it works
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 AUTH_USER_MODEL = 'account.User'
 
